9a88de407febf8e35c90937a052cd66d81551e11
gugatasci <gugatasci@hotmail.com>
2024-11-05 19:18:39 +0100

    added comments

--- src/inlamningsuppgift/RaknaRadTecken.java
+++ src/inlamningsuppgift/RaknaRadTecken.java
@@ -1,20 +1,25 @@
 package inlamningsuppgift;
+
+//Denna klass används för att hålla koll på statistik om användarens inmatning:
+// antalet rader och antalet tecken.
+
 public class RaknaRadTecken {
     private int rowCount;
     private int charCount;
-
+//// Konstruktor som initierar räkningarna till 0
     public RaknaRadTecken() {
         this.rowCount = 0;
         this.charCount = 0;
     }
-
+//Metod som lägger till information om en ny rad.
+//   Om raden INTE är "stop" ökar den rowCount och lägger till antalet tecken
     public void addLine(String line) {
         if (!line.equalsIgnoreCase("stop")) {
             rowCount++;
             charCount += line.length();
         }
     }
-
+    // Returnerar antalet rader o tacken
     public int getRowCount() {
         return rowCount;
     }
--- src/inlamningsuppgift/TextStop.java
+++ src/inlamningsuppgift/TextStop.java
@@ -2,36 +2,46 @@ package inlamningsuppgift;
 
 import java.util.Scanner;
 
+//Den här klassen hanterar användarens inmatning och använder
+// RaknaRadText för att hålla koll på rader och tecken.
+
 public class TextStop{
     private RaknaRadTecken stats;
     private Scanner scanner;
 
+    // Konstruktor som initierar TextStatistics och Scanner
     public TextStop() {
         this.stats = new RaknaRadTecken();
         this.scanner = new Scanner(System.in);
     }
-
+    // Metod för att börja läsa in text från användaren
     public void startReading() {
         System.out.println("Skriv text rad för rad (skriv 'stop' för att avsluta):");
+
+        // En evig loop som fortsätter tills användaren skriver "stop"
+
         while (true) {
             String line = scanner.nextLine();
             if (line.equalsIgnoreCase("stop")) {
                 break;
+                // Om raden är "stop" avbryts loopen
             }
             stats.addLine(line);
+            // Annars läggs raden till statistiken
         }
         displayResults();
+        // Visar resultatet efter loopen avslutats
     }
-
+    // Metod för att skriva ut statistiken
     public void displayResults() {
         System.out.println("Antal rader: " + stats.getRowCount());
         System.out.println("Antal tecken: " + stats.getCharCount());
     }
-
+    // Getter-metod för att hämta statistiken (användbar för tester)
     public RaknaRadTecken getStatistics() {
         return stats;
     }
-
+    // Huvudprogram som startar programmet
     public static void main(String[] args) {
         TextStop reader = new TextStop();
         reader.startReading();
