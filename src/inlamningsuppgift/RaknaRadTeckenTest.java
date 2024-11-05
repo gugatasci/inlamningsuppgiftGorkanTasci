48b61d5027b4dcbbad89e25bd56135b8e07ff86d
gugatasci <gugatasci@hotmail.com>
2024-11-05 19:21:53 +0100

    added comments

--- src/inlamningsuppgift/RaknaRadTeckenTest.java
+++ src/inlamningsuppgift/RaknaRadTeckenTest.java
@@ -8,6 +8,8 @@ package inlamningsuppgift;
 
     public class RaknaRadTeckenTest {
 
+        //Lägger till en rad ("Hello World!") och kontrollerar att radantalet blir 1
+        // och teckenantalet blir 12.
         @Test
         public void testaEnkelRadUtanStop() {
             RaknaRadTecken stats = new RaknaRadTecken();
@@ -15,7 +17,8 @@ package inlamningsuppgift;
             assertEquals(1, stats.getRowCount(), "Row count should be 1 for a single line.");
             assertEquals(12, stats.getCharCount(), "Char count should be 12 for 'Hello World!'.");
         }
-
+//Lägger till tre rader och kontrollerar att rowCount blir 3 och att charCount blir
+// summan av alla tecken (36).
         @Test
         public void testaMangaRaderUtanStop() {
             RaknaRadTecken stats = new RaknaRadTecken();
@@ -25,7 +28,8 @@ package inlamningsuppgift;
             assertEquals(3, stats.getRowCount(), "Row count should be 3 for three lines.");
             assertEquals(36, stats.getCharCount(), "Char count should be the sum of all characters.");
         }
-
+//Lägger till två rader och en tredje rad med ordet "stop". Kontrollera att raden med "stop" ignoreras,
+// så radantalet blir 2 och teckenantalet blir 23.
         @Test
         public void testaEnkelRadMedStop() {
             RaknaRadTecken stats = new RaknaRadTecken();
